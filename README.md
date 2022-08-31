c1bea9fc104fbe84da7cf383da6ca60d93e73ace
Kevin Upton <kevin@soma.finance>
2022-09-01 07:36:49 +1000

    moved to a testing task

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Create 2 Deployer
 
-Deployed Locations
+## Deployed Locations
 
 Rinkeby: [0x4275F0C060Ebe50272f1e483d0E144F83c31d910](https://rinkeby.etherscan.io/address/0x45A1A1a7d02436e0D83a15E177a551F4e8B3a33c)
 Goerli: [0x4275F0C060Ebe50272f1e483d0E144F83c31d910](https://goerli.etherscan.io/address/0x45A1A1a7d02436e0D83a15E177a551F4e8B3a33c)
+
+## Installation
+
+```bash
+yarn add -D create2-deployer
+```
+
+## Usage
--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,15 +1,21 @@
 import {CREATE2_DEPLOYER_ADDRESS} from '../utils';
 import {Create2Deployer__factory} from '../../typechain-types';
 import {ethers, network} from 'hardhat';
+import {task} from 'hardhat/config';
+import {TASK_TEST_SETUP_TEST_ENVIRONMENT} from 'hardhat/builtin-tasks/task-names';
 
-(async () => {
-  const [signer] = await ethers.getSigners();
-  const create2DeployerFactory = new Create2Deployer__factory(signer);
-  const create2Deployer = await create2DeployerFactory.deploy();
-  const bytecode = await ethers.provider.getCode(create2Deployer.address);
+task(TASK_TEST_SETUP_TEST_ENVIRONMENT).setAction(
+  async (opts, hre, runSuper) => {
+    await runSuper();
 
-  await network.provider.send('hardhat_setCode', [
-    CREATE2_DEPLOYER_ADDRESS,
-    bytecode,
-  ]);
-})();
+    const [signer] = await ethers.getSigners();
+    const create2DeployerFactory = new Create2Deployer__factory(signer);
+    const create2Deployer = await create2DeployerFactory.deploy();
+    const bytecode = await ethers.provider.getCode(create2Deployer.address);
+
+    await network.provider.send('hardhat_setCode', [
+      CREATE2_DEPLOYER_ADDRESS,
+      bytecode,
+    ]);
+  }
+);
