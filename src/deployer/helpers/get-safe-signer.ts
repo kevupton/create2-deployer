0d15ef9a405c3f3c1c792a393407bb4c55561aa9
Kevin Upton <kevin@soma.finance>
2023-08-02 14:38:07 +1000

    fixed the deployer

--- src/deployer/helpers/get-safe-signer.ts
+++ src/deployer/helpers/get-safe-signer.ts
@@ -3,17 +3,20 @@ import {ethers, Signer} from 'ethers';
 import Safe, {EthersAdapter} from '@safe-global/protocol-kit';
 import {Provider} from '@ethersproject/providers';
 
-const SAFE_SERVICE_URL =
-  process.env.SAFE_SERVICE_URL || 'https://safe-transaction-goerli.gnosis.io/';
-
 export async function getSafeSigner(
   safeAddress: string,
   signerOrProvider: Signer | Provider
 ) {
-  const service = new SafeService(SAFE_SERVICE_URL);
-  const ethAdapter: any = new EthersAdapter({ethers, signerOrProvider});
+  const service = new SafeService(
+    process.env.SAFE_SERVICE_URL ||
+      'https://safe-transaction-mainnet.safe.global/'
+  );
+  const ethAdapter = new EthersAdapter({
+    ethers,
+    signerOrProvider,
+  });
   const safe = await Safe.create({
-    ethAdapter,
+    ethAdapter: ethAdapter as any,
     safeAddress,
   });
   return SafeEthersSigner.create(
