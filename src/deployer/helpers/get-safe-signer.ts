748f1263162afc78016d0e0fa9cffffc1560f475
Kevin Upton <kevin@soma.finance>
2023-08-02 14:22:56 +1000

    updated ethers adapter

--- src/deployer/helpers/get-safe-signer.ts
+++ src/deployer/helpers/get-safe-signer.ts
@@ -1,7 +1,6 @@
 import {SafeEthersSigner, SafeService} from '@safe-global/safe-ethers-adapters';
-import EthersAdapter from '@safe-global/safe-ethers-lib';
 import {ethers, Signer} from 'ethers';
-import Safe from '@safe-global/protocol-kit';
+import Safe, {EthersAdapter} from '@safe-global/protocol-kit';
 import {Provider} from '@ethersproject/providers';
 
 const SAFE_SERVICE_URL =
@@ -12,7 +11,7 @@ export async function getSafeSigner(
   signerOrProvider: Signer | Provider
 ) {
   const service = new SafeService(SAFE_SERVICE_URL);
-  const ethAdapter = new EthersAdapter({ethers, signerOrProvider});
+  const ethAdapter: any = new EthersAdapter({ethers, signerOrProvider});
   const safe = await Safe.create({
     ethAdapter,
     safeAddress,
